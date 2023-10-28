c52ac7c42d58819776ed9d33e55bcd0afaf46885
himanoa <matsunoappy@gmail.com>
2023-10-29 00:05:34 +0900

    wip

--- client/src/scene/game/component.tsx
+++ client/src/scene/game/component.tsx
@@ -1,7 +1,11 @@
+import { useRef } from 'react'
 import cls from './style.module.css'
+
 export function Game() {
+  const ref = useRef<HTMLCanvasElement>(null)
+
   return (
-    <canvas id="game" className={cls.game}>
+    <canvas id="game" className={cls.game} ref={ref}>
     </canvas>
   )
 }
