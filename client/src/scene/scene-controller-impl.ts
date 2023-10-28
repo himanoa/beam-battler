36a4aaf724966861ab36b3c507e05bf9bbea9a6c
himanoa <matsunoappy@gmail.com>
2023-10-29 00:11:51 +0900

    wip

--- client/src/scene/scene-controller-impl.ts
+++ client/src/scene/scene-controller-impl.ts
@@ -1,6 +1,7 @@
 import { SceneController } from "./scene-controller";
 import { Scene } from ".";
 import PushStream from 'zen-push'
+import Observable from "zen-observable";
 
 export class SceneControllerImpl implements SceneController {
   private stream: PushStream<Scene> = new PushStream()
@@ -12,7 +13,7 @@ export class SceneControllerImpl implements SceneController {
     return this.scene
   }
 
-  get observable() {
+  get observable(): Observable<Scene> {
     return this.stream.observable
   }
 
