ad1fe419b0e4dd592a10013c2d0a995ccabf3406
himanoa <matsunoappy@gmail.com>
2023-10-29 18:08:02 +0900

    Implement render hitbox
    Fix a bug where slip hitbox point

--- client/src/models/player-shooter.ts
+++ client/src/models/player-shooter.ts
@@ -2,7 +2,8 @@ import { match } from "ts-pattern";
 import { Direction } from "./direction";
 import { Renderable } from "./renderable";
 import { Shooter, computeFillColor, getStartPoint, makePointsIter } from "./shooter";
-import { Vector2, addVector } from "./vector2";
+import { Vector2, addVector, divVector } from "./vector2";
+import { HitBox } from "./hit-box";
 
 export class PlayerShooter implements Renderable {
   private shooter: Shooter 
@@ -63,7 +64,7 @@ export class PlayerShooter implements Renderable {
       .exhaustive()
   }
 
-  render(ctx: CanvasRenderingContext2D) {
+  renderShotoer(ctx: CanvasRenderingContext2D) {
     const startPoint = getStartPoint(this.shooter)
 
     ctx.beginPath()
@@ -79,7 +80,36 @@ export class PlayerShooter implements Renderable {
     ctx.fill()
   }
 
+  renderHitBox(ctx: CanvasRenderingContext2D) {
+    ctx.beginPath()
+    ctx.arc(
+      ...this.hitBox.cordinate,
+      this.hitBox.radius,
+      0 * Math.PI / 180,
+      360 * Math.PI / 180,
+      false
+    )
+    ctx.fillStyle = 'white'
+    ctx.strokeStyle = 'red'
+    ctx.stroke()
+    ctx.fill()
+  }
+
+  render(ctx: CanvasRenderingContext2D) {
+    this.renderShotoer(ctx)
+    this.renderHitBox(ctx)
+  }
+
   get id() {
     return this._id
+  }
+
+  get hitBox(): HitBox {
+    const point = divVector(Array.from(makePointsIter(this.shooter)).reduce((acc, value) => addVector(acc, value), [0, 0]),  [3,3])
+
+    return {
+      cordinate: point,
+      radius: 8
+    }
   }
 }
--- client/src/models/vector2.ts
+++ client/src/models/vector2.ts
@@ -1,6 +1,16 @@
 // [x, y]
 export type Vector2 = [number, number]
 
-export const addVector = (v1: Vector2, v2: Vector2): Vector2 => {
-  return [v1[0] + v2[0], v1[1] + v2[1]]
+
+export const map: <T>(source: Vector2, fn: (source: Vector2) => T) => T = (source: Vector2, fn) => {
+  return fn(source)
 }
+
+export const addVector: (v1: Vector2, v2: Vector2) => Vector2 = (v1, v2) =>  {
+  return map(v1, (v1) => [v1[0] + v2[0], v1[1] + v2[1]])
+}
+
+export const divVector: (v1: Vector2, v2: Vector2) => Vector2 = (v1, v2) =>  {
+  return map(v1, (v1) => [v1[0] / v2[0], v1[1] / v2[1]])
+}
+
