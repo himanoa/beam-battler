514fe8de50d21bbc865acec6fd27a2ec96518112
himanoa <matsunoappy@gmail.com>
2023-10-31 03:47:37 +0900

    format

--- server/src/hc.ts
+++ server/src/hc.ts
@@ -1,9 +1,8 @@
-import { Hono } from 'hono'
-import { version } from '../package.json'
+import { Hono } from 'hono';
+import { version } from '../package.json';
 
-export const hc = new Hono()
+export const hc = new Hono();
 
 hc.get('/hc', async (c) => {
-	return c.json({ version })
-})
-
+	return c.json({ version });
+});
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,20 +1,20 @@
-import { Hono } from 'hono'
-import { cors } from 'hono/cors'
-import { logger } from 'hono/logger'
-import { prettyJSON } from 'hono/pretty-json'
+import { Hono } from 'hono';
+import { cors } from 'hono/cors';
+import { logger } from 'hono/logger';
+import { prettyJSON } from 'hono/pretty-json';
 
-import { hc } from './hc'
+import { hc } from './hc';
 
-const app = new Hono()
+const app = new Hono();
 
-app.use('*', prettyJSON())
-app.use('*', logger())
-app.use('*', cors())
+app.use('*', prettyJSON());
+app.use('*', logger());
+app.use('*', cors());
 
 app.get('*', async (c) => {
-	return c.json({}, 404)
-})
+	return c.json({}, 404);
+});
 
-app.route('/', hc)
+app.route('/', hc);
 
-export default app
+export default app;
